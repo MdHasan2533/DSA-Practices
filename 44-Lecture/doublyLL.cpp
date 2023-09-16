76a6ae1fa6f734de682fc3cb2c3942bb9a2653ae
Md Yeah Yea Hasan <107740712+MdHasan2533@users.noreply.github.com>
2023-09-16 07:55:11 +0530

    doubly till insertion

--- 44-Lecture/doublyLL.cpp
+++ 44-Lecture/doublyLL.cpp
@@ -44,24 +44,118 @@ int getLength(Node *head)
     return len;
 }
 
-void insertAtHead(Node *&head, int d)
+void insertAtHead(Node *&head, Node *&tail, int d)
 {
-    Node *temp = new Node(d);
-    temp->next = head;
-    head->prev = temp;
-    head = temp;
+    if (head == NULL)
+    {
+        Node *temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+}
+
+void insertAtTail(Node *&head, Node *&tail, int d)
+{
+    if (tail == NULL)
+    {
+        Node *temp = new Node(d);
+        tail = temp;
+        head = temp;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
+}
+
+void insertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, tail, d);
+        return;
+    }
+    Node *temp = head;
+    int count = 1;
+
+    while (count < position - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    if (temp->next == NULL)
+    {
+        insertAtTail(head, tail, d);
+        return;
+    }
+    // 1st  Creating a node
+    Node *nodeToInsert = new Node(d);
+    // 2nd  nodetoinsert ka next point karado temp k next k sath
+    nodeToInsert->next = temp->next;
+    // 3rd  tem ka next aur uska prev ko point karado nodetoinsert k sath
+    temp->next->prev = nodeToInsert;
+    // 4th temp ka next ko point karado nodetoinsert k sath
+    temp->next = nodeToInsert;
+    // 5th nodetoinsert ka prev point karado temp k sath
+    nodeToInsert->prev = temp;
+    // chaaro pointers point ho gaye
 }
 
 int main()
 {
 
-    Node *node1 = new Node(10);
-    Node *head = node1;
+    Node *head = NULL;
+    Node *tail = NULL;
     print(head);
-    cout << getLength(head) << endl;
+    // cout << getLength(head) << endl;
 
-    insertAtHead(head, 11);
+    insertAtHead(head, tail, 9);
     print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+    insertAtHead(head, tail, 8);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+    insertAtHead(head, tail, 7);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+
+    insertAtTail(head, tail, 13);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+    insertAtTail(head, tail, 14);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+    insertAtTail(head, tail, 15);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+    // cout << getLength(head) << " size" << endl;
+
+    insertAtPosition(tail, head, 3, 100);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+
+    insertAtPosition(tail, head, 2, 110);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
 
     return 0;
 }
